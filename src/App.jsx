011caedceab6ba7e36f2e9befd9d45a8a5a5c237
You Song Hou <ysongweb3@gmail.com>
2023-08-15 01:22:12 -0400

    feat: add some lights to the scene

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   return (
     <div id="canvas-container">
       <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="yellow" position={[0, 0, 5]} />
         <mesh>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial />
