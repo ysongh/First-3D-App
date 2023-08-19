cb361860a70cc7c665feb7c43db53872475ece96
You Song Hou <ysongweb3@gmail.com>
2023-08-19 01:04:43 -0400

    feat: increase the size of the BoxGeometry upon mouse hover

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,6 +4,7 @@ import { useFrame } from '@react-three/fiber';
 function Box() {
   const myMesh = useRef();
   const [isRotate, setRotate] = useState(true);
+  const [hovered, setHover] = useState(false)
 
   useFrame(({ clock }) => {
     const a = clock.getElapsedTime();
@@ -11,7 +12,12 @@ function Box() {
   });
 
   return (
-    <mesh onClick={() => setRotate(!isRotate)} ref={myMesh} scale={2}>
+    <mesh
+      onClick={() => setRotate(!isRotate)}
+      ref={myMesh}
+      scale={hovered ? 1.5 : 1}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[2, 2, 2]} />
       <meshStandardMaterial color="yellow" />
     </mesh>
