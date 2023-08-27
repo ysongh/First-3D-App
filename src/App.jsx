a0d7508906fd89354c5e5950a5809e3732e5cd8b
You Song Hou <ysongweb3@gmail.com>
2023-08-26 22:33:01 -0400

    feat: install @react-three/drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera } from '@react-three/drei';
 
 import Box from './components/Box';
 import MoveBox from './components/MoveBox';
 import Sphere from './components/Sphere';
 import Floor from './components/Floor';
 
+
 function App() {
   return (
     <div id="canvas-container">
       <Canvas>
+        <PerspectiveCamera makeDefault position={[0, 0, 7]}/>
         <ambientLight intensity={0.1} />
         <directionalLight color="white" position={[0, 0, 5]} />
         <Box position={[-1.2, -1, 0]} />
