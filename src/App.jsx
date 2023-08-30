da67c16151c8f9b5175b6349e149863058677e05
You Song Hou <ysongweb3@gmail.com>
2023-08-29 22:24:05 -0400

    refactor: remove one of the boxes

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ function App() {
         <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="white" position={[0, 0, 5]} />
-        <Box position={[-1.2, -1, 0]} />
-        <Box position={[1, 1.5, 0]} />
+        <Box position={[-2.5, 1, 0]} />
         <Sphere position={[2, -1, 0]} />
         <MoveBox />
         <Floor position={[0, -.8, 0]}/>
