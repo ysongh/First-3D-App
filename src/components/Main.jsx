70894fb6265ac69a3dc897ea8ff117dfbc668361
You Song Hou <ysongweb3@gmail.com>
2023-08-31 22:32:36 -0400

    feat: enable camera control to respond to mouse movement

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
 import Box from './Box';
@@ -6,11 +7,31 @@ import MoveBox from './MoveBox';
 import Sphere from './Sphere';
 import Floor from './Floor';
 
+const angleFormat = (deg) => (Math.PI / 180) * deg;
+
 function Main() {
+  const orbitControlsRef = useRef(null);
+
+  useEffect(() => {
+    if(!!orbitControlsRef.current){
+      console.log(orbitControlsRef.current);
+    }
+  }, [orbitControlsRef.current])
+  
+
+  useFrame((state) => {
+    if(!!orbitControlsRef.current){
+      const { x, y } = state.mouse;
+      orbitControlsRef.current.setAzimuthalAngle(-x * angleFormat(45));
+      orbitControlsRef.current.setPolarAngle((y + 1) * angleFormat(90 - 30));
+      orbitControlsRef.current.update();
+    }
+  })
+
   return (
     <>
       <PerspectiveCamera makeDefault position={[0, -1, 8]}/>
-      <OrbitControls />
+      <OrbitControls ref={orbitControlsRef} />
       <ambientLight intensity={0.1} />
       <directionalLight color="white" position={[0, 0, 5]} />
       <Box position={[-2.5, 1, 0]} />
