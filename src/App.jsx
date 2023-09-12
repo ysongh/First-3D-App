3051b4ee9936ed870e50af0cc63e4f5b79cba5e5
You Song Hou <ysongweb3@gmail.com>
2023-09-11 22:16:43 -0400

    refactor: remove the div tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ import Main from './components/Main';
 
 function App() {
   return (
-    <div id="canvas-container">
-      <Canvas shadows>
-        <Main />
-      </Canvas>
-    </div>
+    <Canvas id="canvas-container" shadows>
+      <Main />
+    </Canvas>
   )
 }
 
