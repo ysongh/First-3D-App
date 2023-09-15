fb6793a73f2d665b9cbf4319261d9b0fd0c079ea
You Song Hou <ysongweb3@gmail.com>
2023-09-15 01:41:07 -0400

    refactor: change the color

--- src/components/Floor.jsx
+++ src/components/Floor.jsx
@@ -4,8 +4,8 @@ const angleFormat = (deg) => (Math.PI / 180) * deg;
 function Floor(props) {
   return (
     <mesh {...props} rotation={[-(angleFormat(90)), 0, 0]} receiveShadow>
-      <planeGeometry args={[7, 7]} />
-      <meshStandardMaterial color="pink" />
+      <planeGeometry args={[20, 20]} />
+      <meshStandardMaterial color="#b0d1f4" />
     </mesh>
   )
 }
